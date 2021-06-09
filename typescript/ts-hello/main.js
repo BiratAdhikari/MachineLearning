b63731026c7ffa9f25ae75847626bc742b25b262
biradhik <biradhik@WKWIN6605315>
2021-06-09 10:43:52 +0530

    Angular First

---
+++ typescript/ts-hello/main.js
@@ -0,0 +1,14 @@
+function log(message) {
+    console.log(message);
+}
+var message = "Hello Biraat World";
+log(message);
+var hello = function (num) {
+    {
+        for (var i = 0; i < num; i++) {
+            console.log("number is :", i);
+        }
+    }
+};
+var num = 10;
+hello(num);
---
+++ typescript/ts-hello/main.ts
@@ -0,0 +1,10 @@
+function log (message){
+    console.log(message);
+}
+var message="Hello Biraat World";
+log(message);
+var hello=num=>{{for(let i=0;i<num;i++)
+                    {console.log("number is :",i)}    
+                }}
+let num =10;
+hello(num);                
